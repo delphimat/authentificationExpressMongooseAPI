4e7664f4bd4f08f6ae5e67534de2904d80d0c351
Mathieu Delphin <delphi.mat@gmail.com>
2020-09-20 10:11:02 +0200

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// Define our model
+const userSchema = new Schema({
+
+});
+
+// Create the model class
+
+// Export the model
